9ba7ae122ef1c1c7bc0cfe79867c6832bc17aa7e
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2024-10-16 06:42:27 -0400

    Update happynumber.py

--- happynumber.py
+++ happynumber.py
@@ -39,4 +39,39 @@ class Solution:
         else:
             return False
         
-        exit()
+        # exit()
+#improved version:
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        
+        test = 19
+        def sum_d_sq(test):
+            s_test = str(test)
+            res = 0
+            for each in s_test:
+                # print(each)
+                curr_sq = int(each) ** 2
+                res = res + curr_sq
+                # print(each, curr_sq, sum_d_sq)
+            # print(res)
+            return res
+
+        # sum_d_sq(test)
+        # n = test
+
+        slow, fast = n, sum_d_sq(n)
+        # slow_list = []
+
+        # print(slow, fast, type(slow), type(fast))
+
+        while fast != 1 and fast != slow:
+            slow = sum_d_sq(slow)
+            fast = sum_d_sq(sum_d_sq(fast))
+        
+        if fast == 1:
+            return True
+        else:
+            return False
+        
+        # exit()
+
