005cd073550eb16ee2796c08b7adede14052c218
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2024-10-18 11:08:16 -0400

    Update Linked List Cycle.py

--- Linked List Cycle.py
+++ Linked List Cycle.py
@@ -9,25 +9,18 @@ class Solution:
         # pos represent the index of the node that tail connects with next
         # if pos = -1, no cycle
         #[3,2,0,-4] pos = 1
+        # return False
 
-        # read_head(head):
-        # slow = ListNode(3)
-        # fast = ListNode(3)
-
-        # if slow.val == fast.val:
-        #     print("matching")
-        # exit()
-        print("tail", head.next)
-
-        if not head.next:
-            print("false")
-        
+        if head and head.next is None: # we only return False when the head.val is not None but head.next is None
+            # fast = head.next.next
+            # print(fast.val)
             return False
+        
+        slow, fast = head, head.next.next
 
-        exit()
-        print(slow.val, fast.val)
-        while fast.val != slow.val and slow.next:
+        while fast != slow and fast.next:
             slow = slow.next
             fast = fast.next.next
-            print(slow.val, fast.val)
-            # exit()
+            # print(slow.val, fast.val)
+            #exit()
+        return fast.next is not None  # return True for cycle if fast.next is not None. Otherwise fast finishes all of its moves and no cycle
