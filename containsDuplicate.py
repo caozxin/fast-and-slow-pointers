c3c702384220cad71997573ceb4e7e58864b6ce0
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2024-10-21 15:35:19 -0400

    Update containsDuplicate.py

--- containsDuplicate.py
+++ containsDuplicate.py
@@ -1,1 +1,11 @@
 
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        
+        if not nums:
+            return 
+
+        slow, fast = 0, 0
+
+        while fast < len(nums) and slow != fast:
+            
