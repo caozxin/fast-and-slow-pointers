18505b904d19a823d312448e2e55cb4ddbc54cbb
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2024-10-21 16:00:54 -0400

    Update containsDuplicate.py

--- containsDuplicate.py
+++ containsDuplicate.py
@@ -1,4 +1,3 @@
-
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         
@@ -7,5 +6,22 @@ class Solution:
 
         slow, fast = 0, 0
 
-        while fast < len(nums) and slow != fast:
-            
+        for slow in range(len(nums)):
+
+            # fast = slow + 1
+            # print(slow, fast)
+
+            while fast < len(nums):
+
+                fast += 1
+                # print("fast", fast)
+
+                if fast > len(nums) - 1: #hard enforcement
+                    break
+                if nums[fast] == nums[slow] and fast != slow:
+                    # print("slow, fast", slow, fast)
+                    return True
+            fast = slow + 1
+
+
+        return False
